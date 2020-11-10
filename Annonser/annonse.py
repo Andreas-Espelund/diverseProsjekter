8b5be15f7d4ae05f1cf327bff0a117fc9d751f1c
Andreas-Espelund <andreres@uio.no>
2020-11-10 23:39:24 +0100

    oppdatert scraper

--- Annonser/annonse.py
+++ Annonser/annonse.py
@@ -11,12 +11,20 @@ class Annonse:
     def __str__(self):
         streng = '----'+self._tittel+'----'+'\n'
         streng +='Oppretta: '+self._opprettaDato+'\n'
-        streng += 'Pris: '+self._gjeldandePris+'   Rabatt: '+self._Veilpris-self._gjeldandePris+'\n'
-
+        streng += 'Pris: '+str(self._gjeldandePris)+'   Rabatt: '+str(self._Veilpris-self._gjeldandePris)+'\n'
+        return streng
     def open(self):
         webbrowser.open(self._lenke)
 
+    def endrePris(self,nypris):
+        self._pris = nypris
+
+    def hentPris(self):
+        return self._pris
 
 
-webbrowser.open('https://www.finn.no/bap/forsale/ad.html?finnkode=197443473')
-    
+test= Annonse('Ram','10.11.20',800,'https://www.finn.no/bap/forsale/ad.html?finnkode=197443473')
+
+print(test)
+
+test.open()
--- Annonser/scraper.py
+++ Annonser/scraper.py
@@ -1,2 +1,16 @@
-from bs4 import beautifulsoup
-soup = BeautifulSoup('https://www.finn.no/effekt/197443473', 'html.parser')
+import urllib.request
+import urllib.parse
+import re
+url = 'https://www.finn.no/bap/forsale/ad.html?finnkode=197443473'
+
+
+
+req = urllib.request.Request(url)
+resp = urllib.request.urlopen(req)
+respData = resp.read()
+#print(respData)
+
+paragraphs = re.findall(r'<h1 class = "u-tu">(.*?)</h1>',str(respData))
+
+for par in paragraphs:
+    print(par)
